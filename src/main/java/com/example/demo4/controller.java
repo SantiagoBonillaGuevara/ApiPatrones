249cdd174c25c7a7b50ec61a3e5939c35d143808
dmartin110620 <109118261+dmartin110620@users.noreply.github.com>
2025-02-24 22:21:27 -0500

    return update test

--- src/main/java/com/example/demo4/controller.java
+++ src/main/java/com/example/demo4/controller.java
@@ -28,7 +28,7 @@ public class controller {
         log.info("estoy saludando");
         log.info("estoy saludando");
         log.info("estoy saludando");
-        return "Esta es la prueba para el ArgoCD prueba 6 integracion completa";
+        return "Esta es la prueba para el ArgoCD prueba 7 integracion completa";
     }
 
     @PostMapping("/save")
